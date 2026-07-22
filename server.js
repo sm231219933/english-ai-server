e3e14b25acb5d75c325111d9be3e3e6e232064e6
sm231219933 <sm231219933@gmail.com>
2026-07-22 12:55:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,44 @@
-    const express = require('express');
-    const http = require('http');
-    const socketIo = require('socket.io');
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
-    const app = express();
-    const server = http.createServer(app);
-    const io = socketIo(server, { cors: { origin: "*" } });
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, { cors: { origin: "*" } });
 
-    let waitingUser = null;
+let onlineUsers = {}; // Tracks {socketId: {name, level}}
+let waitingUser = null;
 
-    io.on('connection', (socket) => {
-        socket.on('join-stranger-queue', () => {
-            if (waitingUser && waitingUser !== socket.id) {
-                io.to(waitingUser).emit('match-found', { isInitiator: true });
-                io.to(socket.id).emit('match-found', { isInitiator: false });
-                waitingUser = null;
-            } else {
-                waitingUser = socket.id;
-            }
-        });
-
-        socket.on('send-offer', (data) => socket.broadcast.emit('offer', data));
-        socket.on('send-answer', (data) => socket.broadcast.emit('answer', data));
-        socket.on('send-ice-candidate', (data) => socket.broadcast.emit('ice-candidate', data));
-
-        socket.on('disconnect', () => { if (waitingUser === socket.id) waitingUser = null; });
+io.on('connection', (socket) => {
+    // When a user joins the app
+    socket.on('register-user', (data) => {
+        onlineUsers[socket.id] = { name: data.name, level: data.level, id: socket.id };
+        io.emit('update-user-list', Object.values(onlineUsers)); // Send to everyone
     });
 
-    server.listen(process.env.PORT || 3000, '0.0.0.0', () => {
-        console.log('Cloud Server Running...');
+    socket.on('join-stranger-queue', () => {
+        if (waitingUser && waitingUser !== socket.id) {
+            io.to(waitingUser).emit('match-found', { isInitiator: true });
+            io.to(socket.id).emit('match-found', { isInitiator: false });
+            waitingUser = null;
+        } else {
+            waitingUser = socket.id;
+        }
     });
-    
+
+    socket.on('send-offer', (data) => socket.broadcast.emit('offer', data));
+    socket.on('send-answer', (data) => socket.broadcast.emit('answer', data));
+    socket.on('send-ice-candidate', (data) => socket.broadcast.emit('ice-candidate', data));
+
+    socket.on('disconnect', () => {
+        delete onlineUsers[socket.id];
+        io.emit('update-user-list', Object.values(onlineUsers));
+        if (waitingUser === socket.id) waitingUser = null;
+    });
+});
+
+app.get('/', (req, res) => res.send('Server is Live and Tracking Users!'));
+
+server.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+    console.log('Cloud Server Running...');
+});
